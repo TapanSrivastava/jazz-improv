f363866b3fcc6839b5f7da933fcf7e48fb184ba3
Tapan Srivastava <destinypenguin101@gmail.com>
2015-09-20 15:35:12 -0700

    Initial Commit

---
+++ Chord Analyzer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Chord Analyzer
+//
+//  Created by Tapan Srivastava on 9/20/15.
+//  Copyright © 2015 Tapan Srivastava. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Chord Analyzer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Chord Analyzer
+//
+//  Created by Tapan Srivastava on 9/20/15.
+//  Copyright © 2015 Tapan Srivastava. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
